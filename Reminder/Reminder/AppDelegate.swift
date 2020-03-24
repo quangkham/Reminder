8dd3db84b09fb31cf12e65114a4aa6f47d9fc108
Nan Mauk Quang Kham <nanmaukquangkham@techfun-myanmar.com>
2020-03-24 09:07:09 +0630

    change database name

--- Reminder/Reminder/AppDelegate.swift
+++ Reminder/Reminder/AppDelegate.swift
@@ -42,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "ToDo_Note")
+        let container = NSPersistentContainer(name: "Reminder")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -77,6 +77,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
 
